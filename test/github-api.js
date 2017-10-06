2264fc7c79848b6097f100ad8a9186d3c2fcedd4
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-10-06 11:12:12 +0200

    Déplacement agent

---
+++ test/github-api.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const request = require('superagent');
+const { username, token } = require('../github-credentials.json');
+
+const should = chai.should();
+
+describe('The github API', () => {
+  it('allows me to get a list of pull requests', (done) => {
+    const owner = 'spring-projects';
+    const repo = 'spring-boot';
+    const url = `https://api.github.com/repos/${owner}/${repo}`;
+    request
+      .get(url)
+      .auth(username, token)
+      .set('Accept', 'application/vnd.github.v3+json')
+      .end((err, res) => {
+        should.not.exist(err);
+        should.exist(res);
+        done();
+      });
+  });
+});
+
