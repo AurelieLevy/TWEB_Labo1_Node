d286bfaf383d13f2137e6ff586ef01ceb3253a82
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-10-13 14:54:27 +0200

    MAJ agent pour design de type tree

--- src/agent.js
+++ src/agent.js
@@ -41,21 +41,29 @@ function getCommits(done) {
 
 console.log('Getting commits');
 
-const commitByAuthor = {};
+const commitsTab = {
+  data: [['login', 'taille', 'color', 'message', 'url']],
+};
 
 getCommits((commits) => {
-  // Pour chaque commits, on regarde l'auteur et on compte le nombre de commit total
+  // Pour chaque commit, on récupère l'auteur (commit.author.login), le message
+  // (commit.commit.message), l'url html du commit (commit.html_url)
+
   commits.forEach((commit) => {
     const { login } = commit.author;
-    if (!(login in commitByAuthor)) {
-      commitByAuthor[commit.author.login] = 0;
-    }
+    const { message } = commit.commit;
+    const htmlUrl = commit.html_url;
 
-    commitByAuthor[commit.author.login] += 1;
+    const sizeValue = 1;
+    const colorValue = 0;
+
+    const row = [login, sizeValue, colorValue, message, htmlUrl];
+
+    commitsTab.data.push(row);
   }, this);
 
   // On push sur github
-  console.log('Pushing on github');  
-  githubStorage.publish('docs/repo.json', JSON.stringify(commitByAuthor), 'new version of repo.json', () => { });
+  console.log('Pushing on github');
+  githubStorage.publish('docs/repo.json', JSON.stringify(commitsTab), 'new version of repo.json', () => { });
   console.log('repo.json pushed');
 });
